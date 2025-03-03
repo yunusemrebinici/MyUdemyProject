13301741811197b2ef3eac46ad77c861576f7701
Yunus Emre Binici <biniciyemre@gmail.com>
2025-03-03 11:56:08 +0300

    .

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.DataAccessLayer.EntitiyFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+	public class RegisterController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public RegisterController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
