213f85fffd4bcd08a9e15fbf2548188dde57959f
Yunus Emre Binici <biniciyemre@gmail.com>
2025-03-03 14:49:03 +0300

    .

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_NavbarPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+	public class _NavbarPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View(); 
+		}
+
+
+	}
+}
