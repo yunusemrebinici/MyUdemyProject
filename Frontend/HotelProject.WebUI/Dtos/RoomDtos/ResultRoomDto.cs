00b56e811ed7472c343026682cf95b3e86768db1
Yunus Emre Binici <biniciyemre@gmail.com>
2025-03-04 12:32:50 +0300

    .

---
+++ Frontend/HotelProject.WebUI/Dtos/RoomDtos/ResultRoomDto.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelProject.WebUI.Dtos.RoomDtos
+{
+	public class ResultRoomDto
+	{
+		public int RoomID { get; set; }
+
+		public string RoomNumber { get; set; }
+
+		public string RoomCoverImage { get; set; }
+
+		public int Price { get; set; }
+
+		public string BedCount { get; set; }
+
+		public string BatCount { get; set; }
+
+		public string Description { get; set; }
+	}
+}
