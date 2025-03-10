555b5fa7b52610d07b15a35c495cacaa01fb7309
Yunus Emre Binici <biniciyemre@gmail.com>
2025-03-10 13:09:39 +0300

    .

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+
+        public PartialViewResult _SideBarAdminContactPartial()
+        {
+            return PartialView();
+        }
+
+		public PartialViewResult _SideBarAdminContactCategoryPartial()
+		{
+			return PartialView();
+		}
+	}
+}
