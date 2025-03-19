9634dafe5ffd6a04f6ded7ca8372d32ccd15bfee
Yunus Emre Binici <biniciyemre@gmail.com>
2025-03-19 13:02:39 +0300

    .

--- Frontend/HotelProject.WebUI/Controllers/SettingsController.cs
+++ Frontend/HotelProject.WebUI/Controllers/SettingsController.cs
@@ -6,30 +6,44 @@ using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
-    public class SettingsController : Controller
-    {
-        private readonly UserManager<AppUser> _userManager;
-        public SettingsController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
+	public class SettingsController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+		public SettingsController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            UserEditViewModel userEditViewModel=new UserEditViewModel();
-            userEditViewModel.Name=values.Name;
-            userEditViewModel.Surname=values.Surname;
-            userEditViewModel.Email=values.Email;
-            userEditViewModel.Username = values.UserName;
-            return View(userEditViewModel);
-        }
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var values = await _userManager.FindByNameAsync(User.Identity.Name);
+			UserEditViewModel userEditViewModel = new UserEditViewModel();
+			userEditViewModel.Name = values.Name;
+			userEditViewModel.Surname = values.Surname;
+			userEditViewModel.Email = values.Email;
+			userEditViewModel.Username = values.UserName;
+			return View(userEditViewModel);
+		}
 
-        [HttpPost]
-        public IActionResult Index(string x)
-        {
-            return View();
-        }
-    }
+		[HttpPost]
+		public async Task<IActionResult> Index(UserEditViewModel userEditViewModel)
+		{
+			if (userEditViewModel.Password==userEditViewModel.ConfirmPassword)
+			{
+				var user = await _userManager.FindByNameAsync(User.Identity.Name);
+				user.Name = userEditViewModel.Name;
+				user.Surname = userEditViewModel.Surname;
+				user.Email = userEditViewModel.Email;
+				user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditViewModel.Password);
+				await _userManager.UpdateAsync(user);
+				return RedirectToAction("Index", "Login");
+			}
+			else
+			{
+				return View();
+			}
+			
+		}
+	}
 }
